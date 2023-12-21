54b212b455eb36bfb9d2a071143a2d59836b68dd
Kirill_Dev <alexprogrammer2023@gmail.com>
2023-12-21 10:12:03 +0200

    upload app
    upload app

---
+++ lib/main.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePageScreen(),
+    );
+  }
+}
+
+//Need to explore
+
+/*
+First (or Login) Screen
+*/
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget promoSection = Container(
+      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 7),
+      child: Column(
+        children: [
+          Image.asset('assets/images/login_logo.png'),
+          const Text(
+            'Morning begins with Starbucks coffee',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          children: [
+            promoSection,
+            const SizedBox(
+              height: 60,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomePageScreen()));
+              },
+              icon: const Icon(Icons.email),
+              label: const Text('Login With Email'),
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(300, 50),
+                backgroundColor: const Color(0xff00A6442),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.facebook),
+              label: const Text('Login With Facebook'),
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(300, 50),
+                backgroundColor: const Color(0xff0866FF),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: Image.network('https://www.google.com/favicon.ico'),
+              label: const Text('Login With Google'),
+              style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(300, 50),
+                  backgroundColor: Color(0xffFFFFFFF),
+                  foregroundColor: Color(0xff000000)),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: Image.network('https://www.apple.com/favicon.ico'),
+              label: const Text('Login With Apple'),
+              style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(300, 50),
+                  backgroundColor: Color(0xffffffff),
+                  foregroundColor: Color(0xff000000)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*
+The second screen
+
+ApplicationBar is a custom created appBar that contains two texts and two icons
+*/
+
+
+
+
+Widget MyOwnNavigationBar = Container(
+  padding: EdgeInsets.all(20),
+  child: const Row(
+    children: [
+      Column(
+        children: [
+          Text("Good Morning", style: TextStyle(fontSize: 15),),
+          Text("User", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
+        ],
+      ),
+      Spacer(),
+      Column(
+        children: [
+          Row(
+            children: [
+              Icon(Icons.shopping_bag),
+              Icon(Icons.list)
+            ],
+          ),
+        ],
+      )
+    ],
+  ),
+);
+
+
+class HomePageScreen extends StatelessWidget {
+  const HomePageScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    Widget SearchBar = Container(
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(70),
+        ),
+        shadowColor: Colors.black,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width - 50,
+          height: 50,
+          child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+               children: [
+                    Text("|", style: TextStyle(color: Colors.green, fontSize: 20),),
+                    SizedBox(width: 5),
+                    Text("Seach beverages of foods"),
+                    SizedBox(width: 100),
+                    Icon(Icons.search),
+                  ],
+          ),
+        ),
+      ),
+    );
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+            child: Column(
+          children: [
+            MyOwnNavigationBar,
+            SearchBar
+          ],
+        ),
+        ),
+      ),
+    );
+  }
+}
